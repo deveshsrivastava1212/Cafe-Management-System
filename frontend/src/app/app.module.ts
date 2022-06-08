3c2dbc3de947d06e360e5786e728c8875eb7be96
Vishnu <manav.vivira@gmail.com>
2022-06-08 18:56:40 +0530

    footer name changed

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { SharedModule } from './shared/shared.module';
 import { FullComponent } from './layouts/full/full.component';
 import { AppHeaderComponent } from './layouts/full/header/header.component';
 import { AppSidebarComponent } from './layouts/full/sidebar/sidebar.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [	
     AppComponent,
@@ -30,7 +31,9 @@ import { AppSidebarComponent } from './layouts/full/sidebar/sidebar.component';
     ReactiveFormsModule,
     MaterialModule,
     FlexLayoutModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
