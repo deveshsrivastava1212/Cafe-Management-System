4cbe09b19d78356dc4dfc1ba000ab445e9ebac83
Vishnu <manav.vivira@gmail.com>
2022-06-11 17:00:49 +0530

    auths done

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private router: Router) { }
+  public isAuthenticate(): boolean {
+    const token = localStorage.getItem('token');
+    if(!token){
+      this.router.navigate(['/']);
+      return false;
+    }
+    else{
+      return true;
+    }
+
+  }
 }
