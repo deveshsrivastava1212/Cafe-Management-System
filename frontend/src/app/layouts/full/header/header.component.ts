b4fd72440045917bcc5c84c0aeec04d5675a18ce
Vishnu <manav.vivira@gmail.com>
2022-06-12 08:51:57 +0530

    change password and logout component

--- frontend/src/app/layouts/full/header/header.component.ts
+++ frontend/src/app/layouts/full/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { ChangePasswordComponent } from 'src/app/material-component/dialog/change-password/change-password.component';
+import { ConfirmationComponent } from 'src/app/material-component/dialog/confirmation/confirmation.component';
 
 @Component({
   selector: 'app-header',
@@ -19,7 +21,17 @@ export class AppHeaderComponent {
     dialogConfig.data = {
       message:'Logout'
     };
+    const dialogRef = this.dialog.open(ConfirmationComponent,dialogConfig);
+    const sub = dialogRef.componentInstance.onEmitstatuschange.subscribe((user)=>{
+      dialogRef.close();
+      localStorage.clear();
+      this.router.navigate(['/']);
+    })
+  }
+  changePassword(){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = "550px";
+    this.dialog.open(ChangePasswordComponent,dialogConfig);
     
   }
-
 }
