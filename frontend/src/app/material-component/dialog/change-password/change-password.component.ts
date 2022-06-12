3f272f8b877cc8747fe7670382e3285e8a0e8e65
Vishnu <manav.vivira@gmail.com>
2022-06-12 12:30:49 +0530

    change Password

--- frontend/src/app/material-component/dialog/change-password/change-password.component.ts
+++ frontend/src/app/material-component/dialog/change-password/change-password.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { SnackbarService } from 'src/app/services/snackbar.service';
+import { UserService } from 'src/app/services/user.service';
+import { GlobalConstants } from 'src/app/shared/gloabal-constant';
 
 @Component({
   selector: 'app-change-password',
@@ -6,10 +11,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./change-password.component.scss']
 })
 export class ChangePasswordComponent implements OnInit {
+  changePasswordForm:any = FormGroup;
+  responseMessage:any;
 
-  constructor() { }
+  constructor(private formBuilder:FormBuilder,
+    private userService:UserService,
+    public dialogRef:MatDialogRef<ChangePasswordComponent>,
+    private snackbarService:SnackbarService) { }
 
   ngOnInit(): void {
+    this.changePasswordForm = this.formBuilder.group({
+      oldPassword:[null,[Validators.required]],
+      newPassword:[null,[Validators.required]],
+      confirmPassword:[null,[Validators.required]],
+    })
   }
-
+    validatedSubmit(){
+      if(this.changePasswordForm.controls['newPassword'].value !=this.changePasswordForm.controls['confirmPassword'].value){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+    handleChangePasswordSubmit(){
+      var formData = this.changePasswordForm.value;
+      var data ={
+        oldPassword:formData.oldPassword,
+        newPassword:formData.newPassword,
+        confirmPassword: formData.confirmPassword
+      }
+      this.userService.changePassword(data).subscribe((response:any)=>{
+        this.responseMessage = response?.message;
+        this.dialogRef.close();
+        this.snackbarService.openSnackBar(this.responseMessage,"success");
+      }, (error)=>{
+        console.log(error);
+        if(error.error?.message){
+          this.responseMessage = error.error?.message;
+        }
+        else{
+          this.responseMessage = GlobalConstants.genericError;
+        }
+        this.snackbarService.openSnackBar(this.responseMessage,GlobalConstants.error);
+      })
+    }
 }
