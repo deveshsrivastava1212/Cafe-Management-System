67d7097effc9bf0a2fd4bad6595b31410412a4dd
Vishnu <manav.vivira@gmail.com>
2022-06-13 11:34:23 +0530

    bill service start

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -28,15 +28,21 @@ export class ProductService {
 
   updateStatus(data: any) {
     return this.httpClient.patch(this.url +
-      "/product/updateStatus/",data, {
+      "/product/updateStatus/", data, {
       headers: new HttpHeaders().set('Content-Type', "application/json")
     })
   }
- 
+
   delete(id: any) {
     return this.httpClient.delete(this.url +
-      "/product/delete/"+id, {
+      "/product/delete/" + id, {
       headers: new HttpHeaders().set('Content-Type', "application/json")
     })
+  }
+  getProductsByCategory(id: any) {
+    return this.httpClient.get(this.url + "/product/getByCategory/" + id);
+  }
+  getById(id: any) {
+    return this.httpClient.get(this.url + "/product/getById/" + id);
   }
 }
