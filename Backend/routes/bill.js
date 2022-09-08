3b7515205baabff9813db411adf69e10d3094782
Devesh Srivastava <devesh.srivastava.1212@gmail.com>
2022-09-08 17:34:04 +0530

    fix bug

--- Backend/routes/bill.js
+++ Backend/routes/bill.js
@@ -94,11 +94,11 @@ router.post('/getpdf',auth.authenticateToken,(req,res)=>{
 })
 
 
-router.get('/getBills',auth.authenticateToken,(req,res)=>{
+router.get('/getBills',auth.authenticateToken,(req,res,next)=>{
     var query = "select * from bill order by id DESC";
     connection.query(query,(err,results)=>{
         if(!err){
-            return res.status(200).json(results)
+            return res.status(200).json(results);
         }
         else{
             return res.status(500).json(err);
